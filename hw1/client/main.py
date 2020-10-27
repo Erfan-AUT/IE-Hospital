fb17a4cdd354a807351104e612adcd9d03953abb
Erfan Abedi <erfanabedi.78@gmail.com>
2020-10-27 18:54:34 +0330

    Adding dynamic HTML :))

--- hw1/client/main.py
+++ hw1/client/main.py
@@ -1,11 +1,28 @@
 import requests
 import json
+from enum import Enum
 
-input_str = "start"
+class State(Enum):
+    CHAT_SELECT = 1
+    SENDING_MESSAGE = 2
 
 # while (input_str != "quit"):
-    
-x = requests.get('http://localhost:8000/server/login')
-username = x.headers['username']
+
+jfile = open("info.json", 'r')
+info = json.load(jfile)
+jfile.close()
+if info['username'] == "":
+    x = requests.get('http://localhost:8000/server/login')
+    info['username'] = x.headers['username']
+
 x = requests.get('http://localhost:8000/server/list')
 chat_list = json.loads(x.text)
+
+for index, item in enumerate(chat_list):
+    print("Select one of the following chatrooms:")
+    print(str(index + 1) + ". " + item)
+
+selected_chat = chat_list[int(input()) - 1]
+
+join_headers = {'username': info['username'], 'room': selected_chat}
+x = requests.get('http://localhost:8000/server/join', headers=join_headers)
--- hw1/server/views.py
+++ hw1/server/views.py
@@ -1,35 +1,49 @@
 import json
 import uuid
-from django.utils.dateparse import parse_datetime
 
-from django.http import HttpRequest, HttpResponse, JsonResponse, Http404
-from django.shortcuts import render
 from django.core.serializers import serialize
+from django.http import Http404, HttpRequest, HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.utils.dateparse import parse_datetime
+from django.utils.safestring import mark_safe
+
+from server.models import ChatRoom, Message, User
 from server.serializers import MessageSerializer
 
-from server.models import ChatRoom, User, Message
 
 def __get_user(request: HttpRequest) -> User:
-    user_name = request.headers['username']
+    user_name = request.headers["username"]
     return User.objects.filter(username=user_name)[0]
+
+
+def index(request):
+    return render(request, "chat/index.html", {})
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name))
+    })
 
 
 def login(request: HttpRequest) -> HttpResponse:
     response = HttpResponse()
     username = uuid.uuid4().hex[:8]
-    response['username'] = username
+    response["username"] = username
     user = User(username=username)
     user.save()
     return response
 
+
 def get_rooms_list(request: HttpRequest) -> JsonResponse:
-    all_names = ChatRoom.objects.all().values_list('name', flat=True)
+    all_names = ChatRoom.objects.all().values_list("name", flat=True)
     return JsonResponse(list(all_names), safe=False)
+
 
 def join_room(request: HttpRequest):
     try:
         # last_access = datetime.strptime(request.headers['last_access'])
-        requested_room = request.headers['room']
+        requested_room = request.headers["room"]
         user = __get_user(request)
         room = ChatRoom.objects.filter(name=requested_room)[0]
         user.last_room = room
@@ -41,7 +55,7 @@ def join_room(request: HttpRequest):
 
 def get_messages(request: HttpRequest):
     try:
-        last_access = parse_datetime(request.headers['last-access'])
+        last_access = parse_datetime(request.headers["last-access"])
         user = __get_user(request)
         room = user.last_room
         new_messages = room.message_set.filter(timestamp__gte=last_access)
@@ -60,10 +74,3 @@ def send_message(request: HttpRequest):
         return HttpResponse("Done.")
     except:
         raise Http404()
-
-
-
-
-
-
-
